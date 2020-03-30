989ccde07cf1ab1e6793797012165a8207e3cb7b
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-03-29 23:52:06 -0300

    rollback

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -9,26 +9,22 @@ module.exports = {
         const [count] = await connection('incidents')
             .count();
 
-        if(count.value && count.value > 0){
-            const incidents = await connection('incidents')
-            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
-            .limit(5)
-            .offset((page - 1) * 5 )
-            .select([
-                'incidents.*', 
-                'ongs.name', 
-                'ongs.email', 
-                'ongs.whatsapp', 
-                'ongs.city', 
-                'ongs.uf'
-            ]);
-    
-            response.header('X-Total-Count', count['count(*)']);
-            response.json(incidents);
-        }else{
-            response.header('X-Total-Count', count['count(*)']);
-            response.json([]);
-        }
+        const incidents = await connection('incidents')
+        .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+        .limit(5)
+        .offset((page - 1) * 5 )
+        .select([
+            'incidents.*', 
+            'ongs.name', 
+            'ongs.email', 
+            'ongs.whatsapp', 
+            'ongs.city', 
+            'ongs.uf'
+        ]);
+
+        response.header('X-Total-Count', count['count(*)']);
+        response.json(incidents);
+
     },
 
     async create(request, response) {
